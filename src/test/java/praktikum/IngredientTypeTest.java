2e07a0d5f062b18314d4c89386c769802e4d7656
moni-lilu <moni-lilu@yandex.ru>
2022-08-19 09:36:57 +0100

    task-1

--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -1,2 +1,36 @@
-package praktikum;public class IngredientTypeTest {
+package praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
+
+@RunWith(Parameterized.class)
+public class IngredientTypeTest {
+
+    private final String type;
+    private final IngredientType expectedType;
+
+    public IngredientTypeTest(String type, IngredientType expectedType) {
+        this.type = type;
+        this.expectedType = expectedType;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] checkIngredientType() {
+        return new Object[][] {
+                {"SAUCE", SAUCE},
+                {"FILLING", FILLING}
+        };
+    }
+
+    @Test
+    public void typeShouldBeContainedInEnumIngredientType() {
+        IngredientType actualType = IngredientType.valueOf(type);
+        assertEquals(expectedType, actualType);
+    }
+
 }
